bf10c030e51dbbd9a3fc15319a9a7195a47ba6a6
taka1127 <58457640+taka1127@users.noreply.github.com>
2020-04-22 12:18:06 +0900

    initial_commit

---
+++ src/sample_01/Hero.java
@@ -0,0 +1,26 @@
+package sample_01;
+
+public class Hero {
+	String name;
+	int hp;
+
+	public void attack() {
+
+	}
+
+	public void sleep() {
+
+	}
+
+	public void sit(int sec) {
+
+	}
+
+	public void slip() {
+
+	}
+
+	public void run() {
+
+	}
+}
