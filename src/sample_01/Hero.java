2c015ff7037c267848d5705728a5ffdc6c70a7cc
taka1127 <taka07251127@gmail.com>
2020-04-25 17:18:45 +0900

    day20200425

--- src/sample_01/Hero.java
+++ src/sample_01/Hero.java
@@ -1,36 +1,19 @@
 package sample_01;
 
 public class Hero {
-	String name;
-	int hp;
-	//剣の情報（has-aの関係：Heroは剣を持っている）
-	Sword sword;
+	String name = "ミナト";
+	int hp = 100;
 
-	public void attack() {
-		System.out.println(this.name+ "は"+this.sword.name+"で攻撃した！");
-		System.out.println("敵に"+this.sword.damege+"ポイントのダメージを与えた！");
-	}
-
-	public void sleep() {
-		this.hp = 100;
-		System.out.println(this.name + "は、眠って回復した！");
-	}
-
-	public void sit(int sec) {
-		this.hp += sec;
-		System.out.println(this.name + "は、"+ sec + "秒座った！");
-		System.out.println("HPが"+sec+"ポイント回復し、"+this.hp+"になった！");
-	}
-
-	public void slip() {
-		this.hp -= 5;
-		System.out.println(this.name + "は、転んだ！");
-		System.out.println("5のダメージ！");
+	public void attack(Matango m) {
+		System.out.println(this.name + "の攻撃！");
+		m.hp -= 5;
+		System.out.println("5ポイントのダメージを与えた");
 	}
 
 	public void run() {
-		System.out.println(this.name + "は、逃げ出した！");
-		System.out.println("GAMEOVER");
-		System.out.println("最終HPは" + this.hp + "でした");
+
+		System.out.println(this.name+"は逃げ出した");
 	}
+
+
 }
