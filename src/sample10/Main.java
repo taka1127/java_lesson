ae7b29d2e994dd42de15c156d12c3af96900e103
taka1127 <taka07251127@gmail.com>
2020-05-02 15:10:33 +0900

    20200502

--- src/sample10/Main.java
+++ src/sample10/Main.java
@@ -2,9 +2,25 @@ package sample10;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args)  {
+////		System.out.println("プログラムが起動しました");
+//	    throw new IOException();
+		
+//		FileWriter fw = null;
+//		try {
+//			fw = new FileWriter("data.txt");
+//			fw.write("hello!");
+////			fw.close();
+//		} catch (IOException e) {
+//			System.out.println("書き込み失敗");
+//			
+//		} finally {
+//			fw.close();
+//		}
+		Person p = new Person();
+		p.setAge(-128);
+		System.out.println(p.age);
 	}
+	
 
 }
